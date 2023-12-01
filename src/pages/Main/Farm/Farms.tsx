be1b5e3a0f5bef75e9e0c80dbc24ac298465a67c
sujin <sujin.k0402@gmail.com>
2023-12-02 03:06:50 +0900

    feat: 타일 메뉴추가

--- src/pages/Main/Farm/Farms.tsx
+++ src/pages/Main/Farm/Farms.tsx
@@ -1,6 +1,10 @@
-import styled from 'styled-components';
 import { useEffect, useState } from 'react';
+
+import styled, { css } from 'styled-components';
+import { Menu, MenuItem } from '@mui/material';
+
 import ground from '../../../assets/ground.png';
+import grass from '../../../assets/grass.png';
 
 const StyledFarmGrid = styled.div`
   display: flex;
@@ -10,20 +14,59 @@ const StyledFarmGrid = styled.div`
   width: 660px;
 `;
 
-const StyledGridItem = styled.div`
+const StyledGridItem = styled.div<{ $tile?: string }>`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   width: 160px;
   height: 160px;
-  background-image: url(${ground});
-  border: 1px solid #ccc;
+  border: 0.5px solid #d6b469;
   cursor: pointer;
+  ${({ $tile }) =>
+    $tile &&
+    css`
+      background-image: url(${$tile});
+    `}
 `;
 
 const Farms = () => {
+  const [contextMenu, setContextMenu] = useState<{
+    mouseX: number;
+    mouseY: number;
+  } | null>(null);
   const [gridItems, setGridItems] = useState<JSX.Element[]>([]);
+  const [selectedItem, setSelectedItem] = useState('');
+
+  const handleClickItem = (
+    event: React.MouseEvent<HTMLElement>,
+    item: string,
+  ) => {
+    setContextMenu(
+      contextMenu === null
+        ? {
+            mouseX: event.clientX + 2,
+            mouseY: event.clientY - 6,
+          }
+        : null,
+    );
+    setSelectedItem(item);
+    console.log(item);
+  };
+
+  const handleCloseMenu = () => {
+    setContextMenu(null);
+  };
+
+  const handleClickPlant = () => {
+    console.log('handleClickPlant', selectedItem, import.meta.env.VITE_APP_URL);
+    handleCloseMenu();
+  };
+
+  const handleClickHarvest = () => {
+    console.log('handleClickHarvest', selectedItem);
+    handleCloseMenu();
+  };
 
   useEffect(() => {
     initializeGridItems();
@@ -33,13 +76,38 @@ const Farms = () => {
     const items = [];
     for (let i = 0; i < 4; i++) {
       for (let j = 0; j < 4; j++) {
-        items.push(<StyledGridItem key={`${i}-${j}`}>{i + j}</StyledGridItem>);
+        items.push(
+          <StyledGridItem
+            key={`${i}-${j}`}
+            onClick={(e) => handleClickItem(e, `${i}-${j}`)}
+            $tile={(i + j) % 2 === 0 ? ground : grass}
+          >
+            {i + j}
+          </StyledGridItem>,
+        );
       }
     }
     setGridItems(items);
   };
 
-  return <StyledFarmGrid>{gridItems}</StyledFarmGrid>;
+  return (
+    <>
+      <StyledFarmGrid>{gridItems}</StyledFarmGrid>
+      <Menu
+        open={contextMenu !== null}
+        onClose={handleCloseMenu}
+        anchorReference="anchorPosition"
+        anchorPosition={
+          contextMenu !== null
+            ? { top: contextMenu.mouseY, left: contextMenu.mouseX }
+            : undefined
+        }
+      >
+        <MenuItem onClick={handleClickPlant}>{'심기'}</MenuItem>
+        <MenuItem onClick={handleClickHarvest}>{'수확하기'}</MenuItem>
+      </Menu>
+    </>
+  );
 };
 
 export default Farms;
